167ebe89fff7e64cea451751898984d5e48ffb19
Uri Okhai <uriokhai@gmail.com>
2025-07-28 11:28:28 -0700

    I just wasted my time working on this... aargh

--- src/script.js
+++ src/script.js
@@ -1,18 +1,29 @@
 const Domaccess = (() => {
+  const bodyElem = document.querySelector("body");
   const openMenuElem = document.querySelector(".open-menu");
   const closeMenuElem = document.querySelector(".close-menu");
   const linksDiv = document.querySelector("#nav-links");
-  return {openMenuElem, closeMenuElem, linksDiv}
+  const readMoreBtn = document.querySelector(".read-more");
+  return { bodyElem, openMenuElem, closeMenuElem, linksDiv, readMoreBtn };
 })();
 
-(Domaccess.openMenuElem).addEventListener("click", (e) => {
-  e.target.classList.add("hidden");
-  (Domaccess.linksDiv).style.display = "flex";
-  (Domaccess.closeMenuElem).classList.remove("hidden");
-});
 
-(Domaccess.closeMenuElem).addEventListener("click", (e) => {
-  e.target.classList.add("hidden");
-  (Domaccess.linksDiv).style.display = "none";
-  (Domaccess.openMenuElem).classList.remove("hidden");
-});
+  Domaccess.openMenuElem.addEventListener("click", (e) => {
+if (Domaccess.bodyElem.offsetWidth <= 720) {
+    e.target.style.display = "none";
+    Domaccess.linksDiv.style.display = "flex";
+    Domaccess.closeMenuElem.style.display = "block";
+}else {
+  alert("This feature is available only for small screens");
+}
+  });
+
+  Domaccess.closeMenuElem.addEventListener("click", (e) => {
+    if (Domaccess.bodyElem.offsetWidth <= 720) {
+    e.target.style.display = "none";
+    Domaccess.linksDiv.style.display = "none";
+    Domaccess.openMenuElem.style.display = "block";
+    }else {
+  alert("This feature is available only for small screens");
+}
+  });
