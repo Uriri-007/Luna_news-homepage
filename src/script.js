bd376a28f8f29c0376a79e02a942a4961780e0b3
Uri Okhai <uriokhai@gmail.com>
2025-07-29 08:36:34 +0100

    I fixed the menu bug for mobile devices

--- src/script.js
+++ src/script.js
@@ -1,26 +1,26 @@
+import "./stye.css";
+import "./responsive.css";
+import mobileWeb3Img from "./assets/images/image-web-3-mobile.jpg";
 
 const Domaccess = (() => {
   const bodyElem = document.querySelector("body");
   const openMenuElem = document.querySelector(".open-menu");
   const closeMenuElem = document.querySelector(".close-menu");
   const linksDiv = document.querySelector(".nav-menu");
-  const readMoreBtn = document.querySelector(".read-more");
   const imgElem = document.querySelector(".img-web3");
-  return { bodyElem, openMenuElem, closeMenuElem, linksDiv, readMoreBtn };
+  const containerElem = document.querySelector(".container");
+  return { bodyElem, openMenuElem, closeMenuElem, linksDiv, containerElem };
 })();
 
-//import { mobileWeb3Img } from "./assets/images/image-web-3-mobile.jpg";
 Domaccess.bodyElem.addEventListener("load", () => {
-  console.log(Domaccess.bodyElem.offsetWidth);
-})
-
+  alert(Domaccess.bodyElem.offsetWidth);
+});
 
 Domaccess.openMenuElem.addEventListener("click", (e) => {
   if (Domaccess.bodyElem.offsetWidth <= 720) {
-    e.target.style.display = "none";
     Domaccess.linksDiv.style.display = "flex";
     Domaccess.closeMenuElem.style.display = "block";
-  //  console.log("error")
+    Domaccess
   } else {
     alert("This feature is available only for small screens");
   }
@@ -28,12 +28,7 @@ Domaccess.openMenuElem.addEventListener("click", (e) => {
 
 Domaccess.closeMenuElem.addEventListener("click", (e) => {
   if (Domaccess.bodyElem.offsetWidth <= 720) {
-    e.target.style.display = "none";
     Domaccess.linksDiv.style.display = "none";
-    Domaccess.openMenuElem.style.cssText = `
-    display: block;
-    border: 1px solid black;
-    `;
   } else {
     alert("This feature is available only for small screens");
   }
